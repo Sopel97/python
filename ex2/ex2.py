809d39c16d0a8a8003163149a86858668c206b37
Sopel97 <ts.tomeksopel@gmail.com>
2018-05-21 12:47:53 +0200

    added image display, argparse and improved robustness in ex2

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -2,6 +2,14 @@ import json
 import sys
 import re
 from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+import argparse
+
+def read_json_property(json, key):
+    try:
+        return json[key]
+    except KeyError:
+        raise RuntimeError('No property named {!r} in {!r}'.format(key, json))
 
 class Color:
     tuple_pattern = re.compile(r'\(([0-9]+)[,]([0-9]+)[,]([0-9]+)\)')
@@ -18,8 +26,10 @@ class Color:
         if match:
             return Color(int(match[1], 16), int(match[2], 16), int(match[3], 16))
 
-        if palette:
+        if palette and content in palette:
             return palette[content]
+
+        raise RuntimeError('Invalid color specification {!r}'.format(content))
 
     def __init__(self, r, g, b):
         self._r = r
@@ -62,6 +72,9 @@ class ColorPalette:
     def add_color(self, name, color):
         self._colors[name] = color
 
+    def __contains__(self, key):
+        return key in self._colors
+
     def __getitem__(self, name):
         return self._colors[name]
 
@@ -71,7 +84,7 @@ class Shape:
 class Point(Shape):
     @classmethod
     def from_json(cls, json_node):
-        return Point(json_node['x'], json_node['y'])
+        return Point(read_json_property(json_node, 'x'), read_json_property(json_node, 'y'))
 
     def __init__(self, x, y):
         self._x = x
@@ -101,7 +114,7 @@ class Point(Shape):
 class Polygon(Shape):
     @classmethod
     def from_json(cls, json_node):
-        return Polygon([Point(x, y) for x, y in json_node['points']])
+        return Polygon([Point(x, y) for x, y in read_json_property(json_node, 'points')])
 
     def __init__(self, vertices):
         self._vertices = vertices
@@ -122,7 +135,9 @@ class Rectangle(Shape):
     @classmethod
     def from_json(cls, json_node):
         m = Point.from_json(json_node)
-        return Rectangle(m, m + Point(json_node['width'], json_node['height']))
+        w = read_json_property(json_node, 'width')
+        h = read_json_property(json_node, 'height')
+        return Rectangle(m - Point(w/2, h/2), m + Point(w/2, h/2))
 
     def __init__(self, min, max):
         self._min = min
@@ -146,7 +161,8 @@ class Rectangle(Shape):
 class Square(Shape):
     @classmethod
     def from_json(cls, json_node):
-        return Square(Point.from_json(json_node), json_node['size'])
+        s = read_json_property(json_node, 'size')
+        return Square(Point.from_json(json_node) - Point(s/2, s/2), s)
 
     def __init__(self, min, size):
         self._min = min
@@ -170,7 +186,7 @@ class Square(Shape):
 class Circle(Shape):
     @classmethod
     def from_json(cls, json_node):
-        return Circle(Point.from_json(json_node), json_node['radius'])
+        return Circle(Point.from_json(json_node), read_json_property(json_node, 'radius'))
 
     def __init__(self, origin, radius):
         self._origin = origin
@@ -195,7 +211,7 @@ class Circle(Shape):
 class ColoredShape:
     @classmethod
     def from_json(cls, json_node, shape_cls, palette=None):
-        return ColoredShape(shape_cls.from_json(json_node), Color.from_json(json_node['color'], palette))
+        return ColoredShape(shape_cls.from_json(json_node), Color.from_json(read_json_property(json_node, 'color'), palette))
 
     def __init__(self, shape, color):
         self._shape = shape
@@ -224,7 +240,11 @@ class ShapeList:
         shape_list = ShapeList()
 
         for json_shape_node in json_node:
-            shape_type = ShapeList.shape_types[json_shape_node['type']]
+            shape_name = read_json_property(json_shape_node, 'type')
+            try:
+                shape_type = ShapeList.shape_types[shape_name]
+            except KeyError:
+                raise RuntimeError('Cannot instantiate shape with type {!r}'.format(shape_name))
             colored_shape = ColoredShape.from_json(json_shape_node, shape_type, palette)
             shape_list.add_shape(colored_shape)
 
@@ -250,10 +270,10 @@ class Canvas:
     @classmethod
     def from_json(cls, json_node, palette=None):
         return Canvas(
-            json_node['width'],
-            json_node['height'],
-            Color.from_json(json_node['fg_color'], palette),
-            Color.from_json(json_node['bg_color'], palette)
+            read_json_property(json_node, 'width'),
+            read_json_property(json_node, 'height'),
+            Color.from_json(read_json_property(json_node, 'fg_color'), palette),
+            Color.from_json(read_json_property(json_node, 'bg_color'), palette)
             )
 
     def __init__(self, width, height, fg_color, bg_color):
@@ -278,16 +298,32 @@ class Canvas:
         renderer = ImageDraw.Draw(self._image)
         drawable.draw(renderer)
 
+    def display(self):
+        plt.axis("off")
+        plt.imshow(self._image)
+        plt.show()
+
 def main():
-    json_content = json.load(open(sys.argv[1]))
-    palette = ColorPalette.from_json(json_content['Palette'])
-    canvas = Canvas.from_json(json_content['Screen'], palette)
-    shapes = ShapeList.from_json(json_content['Figures'], palette)
-    print(repr(shapes))
-    print(repr(canvas))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", help="output file (png format)")
+    parser.add_argument("input_file", help="file with shape definitions")
+    args = parser.parse_args()
+
+    json_content = json.load(open(args.input_file))
+
+    try:
+        palette = ColorPalette.from_json(json_content['Palette'])
+        canvas = Canvas.from_json(json_content['Screen'], palette)
+        shapes = ShapeList.from_json(json_content['Figures'], palette)
+    except RuntimeError as e:
+        print(e)
+        sys.exit()
 
     canvas.draw(shapes)
-    canvas.save_to_file('out.png')
+    if args.output:
+        canvas.save_to_file(args.output)
+
+    canvas.display()
 
 
 if __name__ == '__main__':
